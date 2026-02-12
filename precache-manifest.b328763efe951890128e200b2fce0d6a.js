93929bc490dadc68ef9b69cbeb19d9ca3319dc4d
jijek-com <mas.eug@yandex.ru>
2026-02-13 01:22:57 +0500

    Updates

---
+++ precache-manifest.b328763efe951890128e200b2fce0d6a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd94b21a548edf840e58e491b54ba4a7",
+    "url": "/res-countries/index.html"
+  },
+  {
+    "revision": "293cefd672e04236eaef",
+    "url": "/res-countries/static/css/main.f00f7020.chunk.css"
+  },
+  {
+    "revision": "1e573c999f5d38e3b6db",
+    "url": "/res-countries/static/js/2.ea7978a5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/res-countries/static/js/2.ea7978a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293cefd672e04236eaef",
+    "url": "/res-countries/static/js/main.de56dd50.chunk.js"
+  },
+  {
+    "revision": "1782778dcaf099ff9819",
+    "url": "/res-countries/static/js/runtime-main.22940df5.js"
+  },
+  {
+    "revision": "6859113aa8c5228474dd36cd37e81e7a",
+    "url": "/res-countries/static/media/back-white.6859113a.svg"
+  },
+  {
+    "revision": "a744d422bcde045f1e5e998cd0c7779e",
+    "url": "/res-countries/static/media/back.a744d422.svg"
+  },
+  {
+    "revision": "559df9c1cc9b6413b03bf2cdb72b61fb",
+    "url": "/res-countries/static/media/moon-white.559df9c1.svg"
+  },
+  {
+    "revision": "f067b6278439483a68b47c180c1f4863",
+    "url": "/res-countries/static/media/moon.f067b627.svg"
+  },
+  {
+    "revision": "12ec7f05838bfc1e52ce5512dc3103c7",
+    "url": "/res-countries/static/media/search-black.12ec7f05.svg"
+  },
+  {
+    "revision": "f726cd28bcbbde473cf6d44bc63d1f41",
+    "url": "/res-countries/static/media/search.f726cd28.svg"
+  }
+]);
